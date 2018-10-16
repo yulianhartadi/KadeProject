0bb7f85f90cbb79a830c472e237c5c752d745b2d
yulianhartadi <mowvictory@gmail.com>
2018-10-16 16:23:37 +0700

    recyclerview kotlin dicoding academy

--- MainActivity.kt
+++ MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +20,10 @@ class MainActivity : AppCompatActivity() {
         initData()
 
         list.layoutManager = LinearLayoutManager(this)
-        list.adapter = RecyclerViewAdapter(this, items)
+        list.adapter = RecyclerViewAdapter(this, items){
+            val toast  = Toast.makeText(applicationContext, it.name, Toast.LENGTH_SHORT)
+            toast.show()
+        }
 
     }
 
--- RecyclerViewAdapter.kt
+++ RecyclerViewAdapter.kt
@@ -9,14 +9,15 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.squareup.picasso.Picasso
 
-class RecyclerViewAdapter(private val context: Context, private val items: List<Item>)
+class RecyclerViewAdapter(private val context: Context, private val items: List<Item>,
+                          private val listener:(Item) -> Unit)
     : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
             ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_list, parent, false))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindItem(items[position])
+        holder.bindItem(items[position], listener)
     }
 
     override fun getItemCount(): Int = items.size
@@ -26,9 +27,12 @@ class RecyclerViewAdapter(private val context: Context, private val items: List<
         private val name = view.findViewById<TextView>(R.id.name)
         private val image = view.findViewById<ImageView>(R.id.image)
 
-        fun bindItem(items: Item){
+        fun bindItem(items: Item, listener: (Item) -> Unit){
             name.text = items.name
             items.image?.let  {Picasso.get().load(it).into(image)   }
+            itemView.setOnClickListener {
+                listener(items)
+            }
         }
 
     }
